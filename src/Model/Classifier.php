0a761066d6c78da414cb30bcb6d147dd6ea828fa
Yuri <jurager01@gmail.com>
2024-06-28 16:20:44 +0500

    Update Classifier.php

--- src/Model/Classifier.php
+++ src/Model/Classifier.php
@@ -86,7 +86,7 @@ class Classifier extends Simple
      */
     public function getProperties(): PropertyCollection
     {
-        if (empty($this->properties) === 0) {
+        if (empty($this->properties)) {
             $this->properties = new PropertyCollection($this->owner, $this->xml->Свойства);
         }
         return $this->properties;
